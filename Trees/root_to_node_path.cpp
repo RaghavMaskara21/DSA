b3e4414d8647e47c10392997e9a19e9443e681cb
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-02-28 18:28:43 +0530

    root to node path

---
+++ Trees/root_to_node_path.cpp
@@ -0,0 +1,20 @@
+bool path(TreeNode* curr, int B, vector<int> &ans){
+    if(!curr) return false;
+    ans.push_back(curr->val);
+    if(curr->val==B){        
+        return true;
+    }
+    if( path(curr->left,B,ans) || path(curr->right,B,ans))
+    return true;
+    ans.pop_back();
+    return false;
+
+}
+
+vector<int> Solution::solve(TreeNode* A, int B) {
+vector<int> ans;
+if(A==NULL) return ans;
+path(A,B,ans);
+return ans;
+
+}
