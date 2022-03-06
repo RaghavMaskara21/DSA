b1d28fe8231a89e09706f9f28dd5aa8b39bcf028
RaghavMaskara21 <raghavmaskara@gmail.com>
2022-03-06 22:41:36 +0530

    subset sums

---
+++ Recursion_1/Subset_sums.cpp
@@ -0,0 +1,10 @@
+void solve(int ind, vector < int > & arr, int n, vector < int > & ans, int sum) {
+      if (ind == n) {
+        ans.push_back(sum);
+        return;
+      }
+      //element is picked
+      solve(ind + 1, arr, n, ans, sum + arr[ind]);
+      //element is not picked
+      solve(ind + 1, arr, n, ans, sum);
+    }
